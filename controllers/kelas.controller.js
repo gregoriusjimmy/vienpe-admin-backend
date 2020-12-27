4b6fa8bec6a8fbf7e135b52dfd64dad5c4f843c9
Gregorius Jimmy <gregoriusjimmy@gmail.com>
2020-12-27 21:36:11 +0700

    add kelas aktif put controller

--- controllers/kelas.controller.js
+++ controllers/kelas.controller.js
@@ -12,7 +12,7 @@ const handleKelasGet = (req, res, pool) => {
 }
 
 const handleKelasPost = (req, res, pool) => {
-  const { id_instruktur, kategori_senam, hari, jam } = req.body
+  const { id_instruktur, kategori_senam, hari, ja } = req.body
   pool.query(
     `INSERT INTO KELAS (id_instruktur,kategori_senam,hari,jam) VALUES ($1,$2,$3,$4)
     RETURNING id,id_instruktur,kategori_senam,hari,jam,aktif,created_at`,
@@ -27,17 +27,17 @@ const handleKelasPost = (req, res, pool) => {
     }
   )
 }
-const handleMemberPut = (req, res, pool) => {
-  const { id, nama, no_telp, email, tgl_lahir, status_membership } = req.body
+const handleKelasAktifPut = (req, res, pool) => {
+  const { id, aktif } = req.body
   pool.query(
-    'UPDATE member SET nama=$2,no_telp=$3,email=$4,tgl_lahir=$5,status_membership=$6 WHERE id=$1',
-    [id, nama, no_telp, email, tgl_lahir, status_membership],
+    'UPDATE kelas SET aktif=$2 WHERE id=$1 RETURNING id,id_instruktur,kategori_senam,hari,jam,aktif,created_at',
+    [id, aktif],
     (error, results) => {
       if (error) {
         console.log(error)
         res.status(400).json(error.message)
       } else {
-        res.status(200).json('success')
+        res.status(200).json(results.rows[0])
       }
     }
   )
@@ -45,4 +45,5 @@ const handleMemberPut = (req, res, pool) => {
 module.exports = {
   handleKelasGet: handleKelasGet,
   handleKelasPost: handleKelasPost,
+  handleKelasAktifPut: handleKelasAktifPut,
 }
--- routes/kelas.route.js
+++ routes/kelas.route.js
@@ -7,7 +7,7 @@ const kelasRoute = (pool) => {
 
   router.get('/', (req, res) => kelasController.handleKelasGet(req, res, pool))
   router.post('/', (req, res) => kelasController.handleKelasPost(req, res, pool))
-
+  router.put('/aktif',(req,res)=> kelasController.handleKelasAktifPut(req,res,pool))
   return router
 }
 
